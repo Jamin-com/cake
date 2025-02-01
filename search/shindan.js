8d7e4dcb85de5dbf637cb6dfb5034af1d2d75c3d
Jamin-com <m.importantmail@gmail.com>
2025-02-02 01:20:59 +0900

    Update shindan.js

--- search/shindan.js
+++ search/shindan.js
@@ -2,14 +2,14 @@ const cakes = [
  
   { name: "ショートケーキ", description: "こだわりの生クリームにいちごがぴったり！いちごはたっぷり二層",instagramUrl:"https://www.instagram.com/p/DDlYzvyvjjW/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
   { name: "生チョコショート",  description: "チョコレートクリームとバナナの相性ぴったりいちごも乗ってるよ",instagramUrl:"https://www.instagram.com/p/DFNFyXHv2Ap/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="},
-  { name: "紅茶のロールケーキ", description: "紅茶が香るロールケーキ！女性に人気" },
-  { name: "オペラ",  description: "香りのよいコーヒーと上品なビジュアル！上にはチョコレート" },
+  { name: "紅茶のロールケーキ", description: "紅茶アールグレイが香るロールケーキ！女性に人気" },
+  { name: "オペラ",  description: "コーヒーとガナッシュ、特製バタークリームの三重奏" },
   { name: "ジャマイカ",  description: "ラム酒のかおりがコーヒーの風味に合う大人な味",instagramUrl:"https://www.instagram.com/p/DB3L9rhx3cx/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
   { name: "ジャージープリン",  description: "とろとろプリンの上にはおいしい生クリーム！キャラメルソース付き" },
   { name: "ジャージープリン",  description: "美味しいジャージークリームを味わうためのシンプルなロールケーキ！",instagramUrl:"https://www.instagram.com/p/DC6WVytvwSN/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
-  { name: "スフレ", description: "チーズケーキの定番。シンプルなチーズのおいしさが味わえるスフレ" },
+  { name: "スフレ", description: "チーズケーキの定番。シンプルな蔵王チーズのおいしさが味わえるスフレ" },
   { name: "シュークリーム",  description: "カスタードたっぷり毎日出来立てのシュークリーム！" },
-  { name: "ベリーベリーロール【季節限定】",  description: "実は昔のブッシュドノエルを商品化したベリーとチョコレートのロールケーキ" },
+  { name: "ベリーベリーロール【季節限定】",  description: "実は昔のブッシュドノエルを商品化したベリークリームとチョコスポンジのロールケーキ" },
   { name: "すだちのタルト【季節限定】", description: "優しい甘みのメレンゲと酸味のあるすだちが相性抜群のタルト！",instagramUrl:"https://www.instagram.com/p/DBS7y1dPJkB/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
   { name: "タルト・オ・ポンム【季節限定】", description: "ボリューム満点キャラメルとリンゴのオリジナルタルト！",instagramUrl:"https://www.instagram.com/p/DBvk-8ZvMNf/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
   { name: "イチジクの焼きこみタルト【季節限定】", description: "イチジクの香りと甘みを引き出したきれいな焼込みタルト",instagramUrl:"https://www.instagram.com/p/DAsrYspvB5p/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
@@ -21,14 +21,14 @@ const cakes = [
   { name: "厚切りロール",  description: "厚切りのバタークリームロールケーキ！" },
   { name: "ガトーショコラ", description: "チョコレート系王道ガトーショコラ！生クリームも添えて" },
   { name: "ナルシス", description: "アプリコットとレアチーズが相性抜群の一品。チーズは濃厚なフランス産クリームチーズ",instagramUrl:"https://www.instagram.com/p/DCdxmnIR5jR/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
-  { name: "ルモンテ", description: "キャラメルクリームとチョコレートの濃厚だけど重くない絶妙なケーキ！" },
+  { name: "ルモンテ", description: "キャラメルムースとチョコレートの濃厚だけど重くない絶妙なケーキ！" },
   { name: "ユウジュ【季節限定】",  description: "ココナッツと柑橘の組み合わせ！クリームが苦手な方でも食べれます" },
   { name: "ミントエバニーユ",  description: "クレープ生地の中にミントのクリーム" },
   { name: "ジュメル",  description: "かわいらしいビジュアルと中にはいちごのムース！" },
   { name: "バナナキャラメル",  description: "バナナ・チョコレート・キャラメルの濃厚なケーキ！" },
   { name: "レトロフランボワーズ", description: "チョコレートとベリーの組み合わせがお好きな方はこれ！上にはフランボワーズソース" },
-  { name: "フォレノワール", description: "近日公開"  },
-  { name: "カフェオレロール", description: "近日公開"  },
+  { name: "フォレノワール", description: "ちりばめられたグリオットと洋酒キルシュが効いた大人向けチョコケーキ。", instagramUrl:"https://www.instagram.com/p/DFg_XNsPK8C/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
+  { name: "カフェオレロール", description: "チョコスポンジでカフェオレ風味のホワイトチョコクリームを包んだロールケーキ"  },
   { name: "クレームブリュレ",  description: "注文から裏で二度炙り、表面カリカリなかはとろとろ！",instagramUrl:"https://www.instagram.com/p/C_nbFAMPKsx/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
   { name: "ふわふわクレープ",  description: "クレープ生地の中にはいろんなフルーツと生クリーム！" },
   { name: "はっちくんのベイクドレモン",  description: "レモンが香る濃厚で小さいチーズケーキ",instagramUrl:"https://www.instagram.com/p/C_navRIPt8Y/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
@@ -39,7 +39,10 @@ const cakes = [
   { name: "ミックスベリーのタルト【季節限定】",  description: "ベリーたっぷりのタルト！あえてクリームはなくタルトを生かした味わい",instagramUrl:"https://www.instagram.com/p/DFHxMslRX4-/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" }, 
   { name: "安納芋のタルト【季節限定】",  description: "とっても甘いさつまいも安納芋をタルトに！中にはリンゴのコンポート",instagramUrl:"https://www.instagram.com/p/DClnr7fPAPN/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
    { name: "パリブレスト",  description: "シュー生地に濃厚なアーモンドクリームをサンドしたフランス伝統菓子",instagramUrl:"https://www.instagram.com/p/DClVrjVP7aV/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" }, 
-   { name: "レミーマルタン",  description: "洋酒レミーマルタンを味わうためのケーキ。バタークリームとガナッシュ！",instagramUrl:"https://www.instagram.com/p/DEKIAcnPDgW/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },];
+   { name: "レミーマルタン",  description: "洋酒レミーマルタンを味わうためのケーキ。バタークリームとガナッシュ！",instagramUrl:"https://www.instagram.com/p/DEKIAcnPDgW/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
+ { name: "ミルフレーズ",  description: "いちごの層が三層になったショートケーキの進化版！北海道産生クリーム使用。" },
+{ name: "ロワイヤルフレーズ",  description: "いちごムースの上にはホワイトチョコムース。さらにフレッシュないちご！" },
+{ name: "ショコラアムール",  description: "ハート型のガトーショコラの中にフランボワーズとチョコチップを入れ焼きこみました！" },];
 
 const searchBox = document.getElementById('searchBox');
 const cakeList = document.getElementById('cakeList');
